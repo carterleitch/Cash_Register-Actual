302c07850d3f1e12875957bc38f681ae290bc0b7
carterleitch <carterleitchyr11@gmail.com>
2016-10-27 13:33:04 -0400

    Add project files.

---
+++ Cash_Register/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace Cash_Register
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.entranceLabel = new System.Windows.Forms.Label();
+            this.gradeLabel = new System.Windows.Forms.Label();
+            this.gradeInput = new System.Windows.Forms.TextBox();
+            this.testlabel = new System.Windows.Forms.Label();
+            this.gradeDescription = new System.Windows.Forms.Label();
+            this.testDescription = new System.Windows.Forms.Label();
+            this.amountInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.costOutput = new System.Windows.Forms.Label();
+            this.payBox = new System.Windows.Forms.TextBox();
+            this.paidLabel = new System.Windows.Forms.Label();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.changeOutput = new System.Windows.Forms.Label();
+            this.costLabel = new System.Windows.Forms.Label();
+            this.withTax = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.taxOutput = new System.Windows.Forms.Label();
+            this.costWithTaxOutput = new System.Windows.Forms.Label();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.receiptButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.Color.Maroon;
+            this.titleLabel.Font = new System.Drawing.Font("AR BERKLEY", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(97, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(228, 38);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Welcome to CCS";
+            // 
+            // entranceLabel
+            // 
+            this.entranceLabel.AutoSize = true;
+            this.entranceLabel.Font = new System.Drawing.Font("AR BERKLEY", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.entranceLabel.Location = new System.Drawing.Point(118, 47);
+            this.entranceLabel.Name = "entranceLabel";
+            this.entranceLabel.Size = new System.Drawing.Size(175, 25);
+            this.entranceLabel.TabIndex = 1;
+            this.entranceLabel.Text = "Carters Cheat Sheets";
+            // 
+            // gradeLabel
+            // 
+            this.gradeLabel.AutoSize = true;
+            this.gradeLabel.Font = new System.Drawing.Font("AR BERKLEY", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gradeLabel.Location = new System.Drawing.Point(35, 78);
+            this.gradeLabel.Name = "gradeLabel";
+            this.gradeLabel.Size = new System.Drawing.Size(57, 25);
+            this.gradeLabel.TabIndex = 2;
+            this.gradeLabel.Text = "Grade";
+            // 
+            // gradeInput
+            // 
+            this.gradeInput.Location = new System.Drawing.Point(280, 168);
+            this.gradeInput.Name = "gradeInput";
+            this.gradeInput.Size = new System.Drawing.Size(100, 20);
+            this.gradeInput.TabIndex = 3;
+            // 
+            // testlabel
+            // 
+            this.testlabel.AutoSize = true;
+            this.testlabel.Font = new System.Drawing.Font("AR BERKLEY", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.testlabel.Location = new System.Drawing.Point(32, 192);
+            this.testlabel.Name = "testlabel";
+            this.testlabel.Size = new System.Drawing.Size(126, 25);
+            this.testlabel.TabIndex = 4;
+            this.testlabel.Text = "Amount of tests";
+            // 
+            // gradeDescription
+            // 
+            this.gradeDescription.AutoSize = true;
+            this.gradeDescription.Font = new System.Drawing.Font("AR BERKLEY", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gradeDescription.Location = new System.Drawing.Point(89, 113);
+            this.gradeDescription.Name = "gradeDescription";
+            this.gradeDescription.Size = new System.Drawing.Size(274, 42);
+            this.gradeDescription.TabIndex = 5;
+            this.gradeDescription.Text = "We offer a range of cheat sheets streching\r\n from grade 3 all the way to grade 12" +
+    "-13";
+            // 
+            // testDescription
+            // 
+            this.testDescription.AutoSize = true;
+            this.testDescription.Font = new System.Drawing.Font("AR BERKLEY", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.testDescription.Location = new System.Drawing.Point(93, 221);
+            this.testDescription.Name = "testDescription";
+            this.testDescription.Size = new System.Drawing.Size(216, 42);
+            this.testDescription.TabIndex = 6;
+            this.testDescription.Text = "When buyingyour test you can buy\r\nas many as you need";
+            // 
+            // amountInput
+            // 
+            this.amountInput.Location = new System.Drawing.Point(280, 276);
+            this.amountInput.Name = "amountInput";
+            this.amountInput.Size = new System.Drawing.Size(100, 20);
+            this.amountInput.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("AR BERKLEY", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(169, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calculate total";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // costOutput
+            // 
+            this.costOutput.AutoSize = true;
+            this.costOutput.Font = new System.Drawing.Font("AR BERKLEY", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.costOutput.Location = new System.Drawing.Point(229, 353);
+            this.costOutput.Name = "costOutput";
+            this.costOutput.Size = new System.Drawing.Size(0, 21);
+            this.costOutput.TabIndex = 9;
+            // 
+            // payBox
+            // 
+            this.payBox.Location = new System.Drawing.Point(280, 428);
+            this.payBox.Name = "payBox";
+            this.payBox.Size = new System.Drawing.Size(100, 20);
+            this.payBox.TabIndex = 10;
+            // 
+            // paidLabel
+            // 
+            this.paidLabel.AutoSize = true;
+            this.paidLabel.Font = new System.Drawing.Font("AR BERKLEY", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paidLabel.Location = new System.Drawing.Point(87, 427);
+            this.paidLabel.Name = "paidLabel";
+            this.paidLabel.Size = new System.Drawing.Size(88, 21);
+            this.paidLabel.TabIndex = 11;
+            this.paidLabel.Text = "Amount paid";
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.AutoSize = true;
+            this.changeLabel.Font = new System.Drawing.Font("AR BERKLEY", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeLabel.Location = new System.Drawing.Point(119, 495);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(56, 21);
+            this.changeLabel.TabIndex = 12;
+            this.changeLabel.Text = "Change";
+            this.changeLabel.Click += new System.EventHandler(this.changeLabel_Click);
+            // 
+            // changeOutput
+            // 
+            this.changeOutput.AutoSize = true;
+            this.changeOutput.Font = new System.Drawing.Font("AR BERKLEY", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeOutput.Location = new System.Drawing.Point(226, 495);
+            this.changeOutput.Name = "changeOutput";
+            this.changeOutput.Size = new System.Drawing.Size(0, 21);
+            this.changeOutput.TabIndex = 13;
+            // 
+            // costLabel
+            // 
+            this.costLabel.AutoSize = true;
+            this.costLabel.Font = new System.Drawing.Font("AR BERKLEY", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.costLabel.Location = new System.Drawing.Point(139, 353);
+            this.costLabel.Name = "costLabel";
+            this.costLabel.Size = new System.Drawing.Size(38, 21);
+            this.costLabel.TabIndex = 14;
+            this.costLabel.Text = "Cost";
+            // 
+            // withTax
+            // 
+            this.withTax.AutoSize = true;
+            this.withTax.Font = new System.Drawing.Font("AR BERKLEY", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.withTax.Location = new System.Drawing.Point(80, 395);
+            this.withTax.Name = "withTax";
+            this.withTax.Size = new System.Drawing.Size(95, 21);
+            this.withTax.TabIndex = 15;
+            this.withTax.Text = "Cost with tax";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("AR BERKLEY", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(135, 374);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 21);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Tax";
+            // 
+            // taxOutput
+            // 
+            this.taxOutput.AutoSize = true;
+            this.taxOutput.Font = new System.Drawing.Font("AR BERKLEY", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxOutput.Location = new System.Drawing.Point(230, 378);
+            this.taxOutput.Name = "taxOutput";
+            this.taxOutput.Size = new System.Drawing.Size(0, 21);
+            this.taxOutput.TabIndex = 17;
+            // 
+            // costWithTaxOutput
+            // 
+            this.costWithTaxOutput.AutoSize = true;
+            this.costWithTaxOutput.Font = new System.Drawing.Font("AR BERKLEY", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.costWithTaxOutput.Location = new System.Drawing.Point(230, 399);
+            this.costWithTaxOutput.Name = "costWithTaxOutput";
+            this.costWithTaxOutput.Size = new System.Drawing.Size(0, 21);
+            this.costWithTaxOutput.TabIndex = 18;
+            // 
+            // changeButton
+            // 
+            this.changeButton.BackColor = System.Drawing.Color.White;
+            this.changeButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.changeButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.changeButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.changeButton.Font = new System.Drawing.Font("AR BERKLEY", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeButton.ForeColor = System.Drawing.Color.Black;
+            this.changeButton.Location = new System.Drawing.Point(169, 454);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(115, 23);
+            this.changeButton.TabIndex = 19;
+            this.changeButton.Text = "Calculate change";
+            this.changeButton.UseVisualStyleBackColor = false;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // receiptButton
+            // 
+            this.receiptButton.BackColor = System.Drawing.Color.White;
+            this.receiptButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.receiptButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.receiptButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.receiptButton.Font = new System.Drawing.Font("AR BERKLEY", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptButton.ForeColor = System.Drawing.Color.Black;
+            this.receiptButton.Location = new System.Drawing.Point(169, 530);
+            this.receiptButton.Name = "receiptButton";
+            this.receiptButton.Size = new System.Drawing.Size(115, 38);
+            this.receiptButton.TabIndex = 20;
+            this.receiptButton.Text = "Print Receipt";
+            this.receiptButton.UseVisualStyleBackColor = false;
+            this.receiptButton.Click += new System.EventHandler(this.receiptButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("AR BERKLEY", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.ForeColor = System.Drawing.Color.Black;
+            this.resetButton.Location = new System.Drawing.Point(336, 557);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(110, 48);
+            this.resetButton.TabIndex = 21;
+            this.resetButton.Text = "New Order";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(760, 608);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.receiptButton);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.costWithTaxOutput);
+            this.Controls.Add(this.taxOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.withTax);
+            this.Controls.Add(this.costLabel);
+            this.Controls.Add(this.changeOutput);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.paidLabel);
+            this.Controls.Add(this.payBox);
+            this.Controls.Add(this.costOutput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.amountInput);
+            this.Controls.Add(this.testDescription);
+            this.Controls.Add(this.gradeDescription);
+            this.Controls.Add(this.testlabel);
+            this.Controls.Add(this.gradeInput);
+            this.Controls.Add(this.gradeLabel);
+            this.Controls.Add(this.entranceLabel);
+            this.Controls.Add(this.titleLabel);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.Text = "CCS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label entranceLabel;
+        private System.Windows.Forms.Label gradeLabel;
+        private System.Windows.Forms.TextBox gradeInput;
+        private System.Windows.Forms.Label testlabel;
+        private System.Windows.Forms.Label gradeDescription;
+        private System.Windows.Forms.Label testDescription;
+        private System.Windows.Forms.TextBox amountInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label costOutput;
+        private System.Windows.Forms.TextBox payBox;
+        private System.Windows.Forms.Label paidLabel;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Label changeOutput;
+        private System.Windows.Forms.Label costLabel;
+        private System.Windows.Forms.Label withTax;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label taxOutput;
+        private System.Windows.Forms.Label costWithTaxOutput;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Button receiptButton;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ Cash_Register/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Cash_Register
+{
+    public partial class Form1 : Form
+    {
+        const double SHEETS_COST = 2.25;
+        const double TEST_COSTS = 3.75;
+        const double TAX_AMOUNT = 0.13;
+
+        int tests, sheets, amountPaid;
+
+        double price, taxAmount, totalWithTax, changeDue, sheetprice, testprice;
+
+        private void changeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            sheets = Convert.ToInt32(gradeInput.Text);
+            tests = Convert.ToInt32(amountInput.Text);
+
+            sheetprice = SHEETS_COST * sheets;
+            testprice = TEST_COSTS * tests;
+            price = SHEETS_COST * sheets + TEST_COSTS * tests;
+            taxAmount = TAX_AMOUNT * price;
+            totalWithTax = taxAmount + price;
+            
+            costOutput.Text = price.ToString("C");
+            taxOutput.Text = taxAmount.ToString("C");
+            costWithTaxOutput.Text = totalWithTax.ToString("C");
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+            amountPaid = Convert.ToInt32(payBox.Text);
+            changeDue = amountPaid - totalWithTax;
+            changeOutput.Text = changeDue.ToString("C");
+        }
+        private void receiptButton_Click(object sender, EventArgs e)
+        {
+            Graphics FormGraphics = this.CreateGraphics();
+            Pen firstpen = new Pen(Color.Black, 10);
+            Pen Secondpen = new Pen(Color.Maroon, 10);
+            SolidBrush drawbrush = new SolidBrush(Color.White);
+            SolidBrush Blackbrush = new SolidBrush(Color.Black);
+            SolidBrush Redbrush = new SolidBrush(Color.Red);
+            SolidBrush titlebrush = new SolidBrush(Color.Maroon);
+            Font drawfont = new Font("Consolas", 12, FontStyle.Bold);
+            Font titlefont = new Font("Consolas", 27);
+
+            FormGraphics.FillRectangle(drawbrush, 400, 50, 300, 500);
+            FormGraphics.DrawLine(firstpen, 400, 50, 700, 50);
+            FormGraphics.DrawLine(firstpen, 400, 45, 400, 555);
+            FormGraphics.DrawLine(firstpen, 700, 45, 700, 555);
+            FormGraphics.DrawLine(firstpen, 400, 550, 700, 550);
+
+            FormGraphics.DrawString("CCS", titlefont, titlebrush, 520, 60);
+            FormGraphics.DrawString("Thank you for shopping at CSS", drawfont, Blackbrush, 420, 90);
+            FormGraphics.DrawString("Products", drawfont, Blackbrush, 420, 120);
+            FormGraphics.DrawString("    Sheets     X" + sheets, drawfont, Blackbrush, 420, 170);
+            FormGraphics.DrawString("                       " + sheetprice.ToString("C"), drawfont, Blackbrush, 420, 170);
+            FormGraphics.DrawString("    Tests      X" + tests, drawfont, Blackbrush, 420, 190);
+            FormGraphics.DrawString("                       " + testprice.ToString("C"), drawfont, Blackbrush, 420, 190);
+            FormGraphics.DrawString("Total without tax      " + price.ToString("C"), drawfont, Blackbrush, 420, 250);
+            FormGraphics.DrawString("Tax                    " + taxAmount.ToString("C"), drawfont, Blackbrush, 420, 270);
+            FormGraphics.DrawString("Total Cost             " + totalWithTax.ToString("C"), drawfont, Blackbrush, 420, 290);
+            FormGraphics.DrawString("Tendered               " + amountPaid.ToString("C"), drawfont, Blackbrush, 420, 330);
+            FormGraphics.DrawString("Change                 " + changeDue.ToString("C"), drawfont, Blackbrush, 420, 350);
+            FormGraphics.DrawString("have a nice day", drawfont, Blackbrush, 480, 450);
+
+        }
+            private void resetButton_Click(object sender, EventArgs e)
+        {
+            Graphics FormGraphics = this.CreateGraphics();
+            Pen Secondpen = new Pen(Color.Maroon, 10);
+            SolidBrush titlebrush = new SolidBrush(Color.Maroon);
+
+            FormGraphics.FillRectangle(titlebrush, 395, 45, 310, 510);
+
+            costOutput.Text = " ";
+            taxOutput.Text = " ";
+            costWithTaxOutput.Text = " "; 
+            changeOutput.Text = " ";
+            payBox.Text = " ";
+            gradeInput.Text = " ";
+            amountInput.Text = " ";
+
+            tests = 0;
+            sheets = 0;
+            testprice = 0;
+            sheetprice = 0;
+            price = 0;
+            taxAmount = 0;
+            totalWithTax = 0;
+            amountPaid = 0;
+            changeDue = 0;
+        }
+    }
+}
